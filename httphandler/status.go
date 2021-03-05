1b4be51643776835c96b15d139f98c10f62296c6
Stephan Protsack <stephan@speedandfunction.com>
2021-03-05 12:55:43 +0200

    Fix comments

--- httphandler/status.go
+++ httphandler/status.go
@@ -8,17 +8,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// StatusResponse status codes response
+// StatusResponse health check response
 type StatusResponse struct {
 	Uptime int               `json:"uptime"`
 	Online map[string]bool   `json:"online"`
 	Errors map[string]string `json:"errors"`
 }
 
-// StatusCheck checking status of the service
+// StatusCheck check status of the service
 type StatusCheck func(ctx context.Context) error
 
-// Status get application status
+// Status health check API endpoint
 func Status(services map[string]StatusCheck) gin.HandlerFunc {
 	startup := time.Now().UTC()
 
