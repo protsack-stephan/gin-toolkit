50691cb7cef45ce4f122bc1681608aa71d47c051
Stephan Protsack <stephan@speedandfunction.com>
2021-03-05 12:53:36 +0200

    Add status handler

---
+++ httphandler/status.go
@@ -0,0 +1,44 @@
+package httphandler
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// StatusResponse status codes response
+type StatusResponse struct {
+	Uptime int               `json:"uptime"`
+	Online map[string]bool   `json:"online"`
+	Errors map[string]string `json:"errors"`
+}
+
+// StatusCheck checking status of the service
+type StatusCheck func(ctx context.Context) error
+
+// Status get application status
+func Status(services map[string]StatusCheck) gin.HandlerFunc {
+	startup := time.Now().UTC()
+
+	return func(c *gin.Context) {
+		res := StatusResponse{
+			Uptime: int(time.Since(startup).Seconds()),
+			Errors: map[string]string{},
+			Online: map[string]bool{},
+		}
+
+		for name, ping := range services {
+			err := ping(c)
+
+			if err != nil {
+				res.Errors[name] = err.Error()
+			}
+
+			res.Online[name] = err == nil
+		}
+
+		c.JSON(http.StatusOK, res)
+	}
+}
