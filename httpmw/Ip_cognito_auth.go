94c211787134b182a05ba4825d70e95376d868d0
Alexander Laurie <alexander.l@speedandfunction.com>
2021-12-29 07:30:15 -0500

    Check ClientID from token
    Compare ClientID from access_token with current ClientID

--- httpmw/Ip_cognito_auth.go
+++ httpmw/Ip_cognito_auth.go
@@ -1,18 +1,20 @@
 package httpmw
 
 import (
+	"log"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider/cognitoidentityprovideriface"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"github.com/protsack-stephan/gin-toolkit/httperr"
-	"log"
-	"strings"
 )
 
 // IpCognitoAuth middleware for:
 // * IP ranges verification in format "192.168.10.1-192.168.10.10,192.168.90.1-192.168.90.10"
 // * cognito authentication through Authorization Bearer Token
-func IpCognitoAuth(ipRange string, svc cognitoidentityprovideriface.CognitoIdentityProviderAPI) gin.HandlerFunc {
+func IpCognitoAuth(ipRange string, svc cognitoidentityprovideriface.CognitoIdentityProviderAPI, cogntoClientID string) gin.HandlerFunc {
 	ipRanges := getIpRanges(ipRange)
 
 	return func(c *gin.Context) {
@@ -27,6 +29,25 @@ func IpCognitoAuth(ipRange string, svc cognitoidentityprovideriface.CognitoIdent
 		token := strings.Replace(c.GetHeader("Authorization"), "Bearer ", "", 1)
 
 		if len(token) <= 0 {
+			httperr.Unauthorized(c)
+			c.Abort()
+			return
+		}
+
+		jwtToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+			return []byte{}, nil
+		})
+
+		if err != nil {
+			log.Println(err)
+			httperr.Unauthorized(c)
+			c.Abort()
+			return
+		}
+
+		claims, ok := jwtToken.Claims.(jwt.MapClaims)
+
+		if !ok || claims["client_id"] != cogntoClientID {
 			httperr.Unauthorized(c)
 			c.Abort()
 			return
--- httpmw/Ip_cognito_auth_test.go
+++ httpmw/Ip_cognito_auth_test.go
@@ -3,18 +3,21 @@ package httpmw
 import (
 	"errors"
 	"fmt"
-	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
-	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider/cognitoidentityprovideriface"
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider/cognitoidentityprovideriface"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-const authTestToken = "token-sg332f"
 const authTestUsername = "alex_name"
+const authTestCogntoClientId = "client-id-123123123"
+const authTestCogntoWrongClientId = "client-id-123123124"
 
 type cognitoIdentityProviderClientMock struct {
 	cognitoidentityprovideriface.CognitoIdentityProviderAPI
@@ -27,11 +30,22 @@ func (c *cognitoIdentityProviderClientMock) GetUser(input *cognitoidentityprovid
 	return args.Get(0).(*cognitoidentityprovider.GetUserOutput), args.Error(1)
 }
 
+func GetJWTtoken() (token string) {
+	claims := jwt.MapClaims{
+		"client_id": authTestCogntoClientId,
+	}
+
+	jwttoken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, _ := jwttoken.SignedString([]byte{})
+
+	return tokenString
+}
+
 func TestCognitoIPSucceed(t *testing.T) {
 	assert := assert.New(t)
 	ipRanges := "192.168.10.1-192.168.10.10,192.168.20.1-192.168.20.10"
 	router := gin.New()
-	router.Use(IpCognitoAuth(ipRanges, &cognitoIdentityProviderClientMock{}))
+	router.Use(IpCognitoAuth(ipRanges, &cognitoIdentityProviderClientMock{}, authTestCogntoClientId))
 	router.GET("/login", func(c *gin.Context) {
 		c.Status(http.StatusOK)
 	})
@@ -47,7 +61,7 @@ func TestCognitoIP401(t *testing.T) {
 	assert := assert.New(t)
 	ipRanges := "192.168.10.1-192.168.10.10,192.168.20.1-192.168.20.10"
 	router := gin.New()
-	router.Use(IpCognitoAuth(ipRanges, &cognitoIdentityProviderClientMock{}))
+	router.Use(IpCognitoAuth(ipRanges, &cognitoIdentityProviderClientMock{}, authTestCogntoClientId))
 	router.GET("/login", func(c *gin.Context) {
 		called = true
 
@@ -66,7 +80,7 @@ func TestCognitoIP401(t *testing.T) {
 func TestCognitoIpAuth(t *testing.T) {
 	assert := assert.New(t)
 	ipRanges := "192.168.20.1-192.168.20.10"
-	token := authTestToken
+	token := GetJWTtoken()
 	username := authTestUsername
 	router := gin.New()
 
@@ -80,7 +94,7 @@ func TestCognitoIpAuth(t *testing.T) {
 			nil,
 		)
 
-	router.Use(IpCognitoAuth(ipRanges, srv))
+	router.Use(IpCognitoAuth(ipRanges, srv, authTestCogntoClientId))
 	router.GET("/login", func(c *gin.Context) {
 		uname, _ := c.Get("username")
 
@@ -94,11 +108,42 @@ func TestCognitoIpAuth(t *testing.T) {
 	assert.Equal(http.StatusOK, w.Code)
 }
 
+func TestCognitoIpAuthTokenFails(t *testing.T) {
+	assert := assert.New(t)
+	ipRanges := "192.168.20.1-192.168.20.10"
+	token := GetJWTtoken()
+	username := authTestUsername
+	router := gin.New()
+
+	srv := new(cognitoIdentityProviderClientMock)
+	srv.
+		On("GetUser", &cognitoidentityprovider.GetUserInput{AccessToken: &token}).
+		Return(
+			&cognitoidentityprovider.GetUserOutput{
+				Username: &username,
+			},
+			nil,
+		)
+
+	router.Use(IpCognitoAuth(ipRanges, srv, authTestCogntoWrongClientId))
+	router.GET("/login", func(c *gin.Context) {
+		uname, _ := c.Get("username")
+
+		assert.Equal(authTestUsername, *uname.(*string))
+		c.Status(http.StatusOK)
+	})
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/login", nil)
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", "Bearer", token))
+	router.ServeHTTP(w, req)
+	assert.Equal(http.StatusUnauthorized, w.Code)
+}
+
 func TestCognitoIpAuthFails(t *testing.T) {
 	assert := assert.New(t)
 	called := false
 	ipRanges := "192.168.20.1-192.168.20.10"
-	token := authTestToken
+	token := GetJWTtoken()
 	router := gin.New()
 
 	srv := new(cognitoIdentityProviderClientMock)
@@ -109,7 +154,7 @@ func TestCognitoIpAuthFails(t *testing.T) {
 			errors.New("token is not valid"),
 		)
 
-	router.Use(IpCognitoAuth(ipRanges, srv))
+	router.Use(IpCognitoAuth(ipRanges, srv, authTestCogntoClientId))
 	router.GET("/login", func(c *gin.Context) {
 		called = true
 		c.Status(http.StatusOK)
