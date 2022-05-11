54af53bd6e92f0b4de1e80d685f95fd617ac4ced
Daniel Memije <hello@memije.dev>
2022-05-11 04:48:31 -0500

    Added custom Log Formatter (#15)
    * Added custom Log Formatter
    
    Added custom log formatter for gin logging middleware.
    This formatter will create a log entry in JSON format with the
    following information:
    
    - Request's timestamp in RFC3339 format
    - Client's IP address
    - Consumed API path/endpoint
    - Request's response status code
    
    It will also include the following fields if a `CognitoCache` instance
    is in the gin's context:
    
    - Username
    - User's groups
    
    * Fixed comments on logging test
    
    - Added `apiPath` and `testIP` as constants
    - Removed redundant error assignment when doing json unmarshaling

---
+++ httpmw/logging_test.go
@@ -0,0 +1,89 @@
+package httpmw
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogFormatter(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	assert := assert.New(t)
+
+	t.Run("test log formatting function", func(_ *testing.T) {
+		const apiPath = "/test"
+		const testIP = "0.0.0.0"
+
+		t.Run("test without cognito user middleware", func(_ *testing.T) {
+			e := gin.New()
+			out := new(bytes.Buffer)
+
+			e.Use(gin.LoggerWithConfig(gin.LoggerConfig{
+				Formatter: LogFormatter,
+				Output:    out,
+			}))
+
+			e.GET(apiPath, func(c *gin.Context) {
+				c.Status(http.StatusTeapot)
+			})
+
+			req, err := http.NewRequest(http.MethodGet, apiPath, nil)
+			assert.NoError(err)
+			req.Header.Add("X-Forwarded-For", testIP)
+
+			w := httptest.NewRecorder()
+			e.ServeHTTP(w, req)
+			assert.Equal(http.StatusTeapot, w.Code)
+
+			entry := new(logEntry)
+			assert.NoError(json.Unmarshal(out.Bytes(), entry))
+
+			assert.NotEmpty(entry.RequestTime)
+			assert.Equal(testIP, entry.Ip)
+			assert.Equal(apiPath, entry.Path)
+			assert.Equal(w.Code, entry.Status)
+		})
+
+		t.Run("test with cognito user middleware", func(_ *testing.T) {
+			e := gin.New()
+			out := new(bytes.Buffer)
+
+			testUser := &CognitoUser{
+				Username: "test_username",
+				Groups:   []string{"group_1"},
+			}
+
+			e.Use(func(c *gin.Context) {
+				c.Set("user", testUser)
+			})
+
+			e.Use(gin.LoggerWithConfig(gin.LoggerConfig{
+				Formatter: LogFormatter,
+				Output:    out,
+			}))
+
+			e.GET(apiPath, func(c *gin.Context) {
+				c.Status(http.StatusTeapot)
+			})
+
+			req, err := http.NewRequest(http.MethodGet, apiPath, nil)
+			assert.NoError(err)
+			req.Header.Add("X-Forwarded-For", testIP)
+
+			w := httptest.NewRecorder()
+			e.ServeHTTP(w, req)
+			assert.Equal(http.StatusTeapot, w.Code)
+
+			entry := new(logEntry)
+			assert.NoError(json.Unmarshal(out.Bytes(), entry))
+
+			assert.Equal(testUser.Username, entry.Username)
+			assert.ElementsMatch(testUser.Groups, entry.UserGroups)
+		})
+	})
+}
